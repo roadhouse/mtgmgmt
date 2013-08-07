86324286f2ae713bcb6ccdd0ff2c86b298243fe9
Jean Uchoa <poserpride@gmail.com>
2013-08-07 03:15:44 -0300

    update jquery and add select2

--- db/migrate/20130527022141_create_inventories.rb
+++ db/migrate/20130527022141_create_inventories.rb
@@ -2,7 +2,8 @@ class CreateInventories < ActiveRecord::Migration
   def change
     create_table :inventories do |t|
       t.integer :quantity
-      t.references :card, :user
+      t.references :card, :user, :language
+      t.boolean :foil
       t.timestamps
     end
   end
