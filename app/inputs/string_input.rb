e1bd43d75c4b5f6fadab87499a5d338f76fcf2e6
Jean Uchoa <poserpride@gmail.com>
2013-08-12 21:07:52 -0300

    remove simple_form customs

--- app/inputs/string_input.rb
+++
@@ -1,7 +0,0 @@
-class StringInput < SimpleForm::Inputs::StringInput
-  def input
-    @options[:placeholder] = attribute_name.to_s.titleize if options[:placeholder].blank?
-    @options[:label] = false
-    super
-  end
-end
