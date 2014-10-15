3b36ab601dd46ede5cb8b4da2deadeff5f2b71e5
Jean Uchoa <jean.uchoa@gmail.com>
2014-10-15 19:57:48 +0000

    add lib to autoload

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ end
 
 module Magic
   class Application < Rails::Application
-    config.autoload_paths += %W(#{config.root}/lib/app/laracna)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
     config.encoding = "utf-8"
     config.filter_parameters += [:password]
     config.active_support.escape_html_entities_in_json = true
