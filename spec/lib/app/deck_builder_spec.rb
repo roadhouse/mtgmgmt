6409fb34453aa4c68ec32dfb2a5be950403916d2
Jean Uchoa <jean.uchoa@gmail.com>
2014-10-15 20:02:23 +0000

    move stuff

--- spec/lib/app/deck_builder_spec.rb
+++
@@ -1,8 +0,0 @@
-require "ostruct"
-require "factory_girl"
-
-require "./lib/app/deck_builder"
-require "./lib/app/deck_list_parser"
-
-describe DeckBuilder do
-end
--- spec/lib/app/laracna/deck_lists_crawler_spec.rb
+++
@@ -1,9 +0,0 @@
-require "./spec/support/vcr"
-
-# require "./lib/app/laracna/deck_lists/deck_lists_crawler"
-
-class DeckListsCrawler; end
-
-describe DeckListsCrawler, :vcr do
-
-end
