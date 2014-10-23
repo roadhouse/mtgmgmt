38a08fe41e58b9cf6ee5cdb6ab15cb9090446640
Jean Uchoa <jean.uchoa@gmail.com>
2014-10-23 15:28:21 +0000

    [tdd][green] method implementation

--- lib/mana_cost.rb
+++ lib/mana_cost.rb
@@ -6,4 +6,24 @@ class ManaCost
   def value
     @mana_cost.match(/{(\d)}/).captures.first.to_i
   end
+
+  def colorless?
+    [black?, red?, blue?, green?, white?].all? {|color| color == false}
+  end
+
+  def black?
+    false
+  end
+  def red?
+    false
+  end
+  def blue?
+    false
+  end
+  def green?
+    false
+  end
+  def white?
+    false
+  end
 end
--- spec/lib/mana_cost_spec.rb
+++ spec/lib/mana_cost_spec.rb
@@ -6,5 +6,12 @@ describe ManaCost do
       subject { ManaCost.new("{3}").value }
       it { is_expected.to eql 3 }
     end
+
+    context ".colorless?" do
+      subject { ManaCost.new("{3}").colorless? }
+
+      it { is_expected.to be_truthy }
+    end
+
   end
 end
