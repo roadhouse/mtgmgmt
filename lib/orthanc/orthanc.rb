55406f59eef1c9abaddecb116851b39bc3302838
Jean Uchoa <jean.uchoa@gmail.com>
2014-11-06 18:02:41 -0200

    update sqls

--- lib/orthanc/orthanc.rb
+++ lib/orthanc/orthanc.rb
@@ -3,26 +3,58 @@ class Orthanc
     @cards = cards
   end
 
-  def most_playable_cards(limit = 20)
-    lands_ids             = Card.per_type("land").map(&:id)
-    more_playable_to_less = "quantity desc"
-    quantity              = "count(card_id) as quantity"
-
-    @cards
-      .select(:card_id, :part, quantity)
-      .group(:part, :card_id)
-      .order(more_playable_to_less)
-      .having('card_id not in (?)', lands_ids)
-      .limit(limit)
+  # .test(color: :w, type: :c)
+  def teste(p={limit: 10})
+    Card.joins(:card_decks)
+      .select(:name, Card.arel_table[:name].count.as("quantity"))
+      .where(params(p))
+      .group("cards.name")
+      .order("quantity desc")
+      .limit(p.fetch(:limit) { 10})
   end
 
-  def most_playable_decks(limit = 15)
-    more_playable_to_less = "quantity desc"
-    quantity              = "count(name) as quantity"
+  def params(p)
+    Hash.new.tap do |where|
+      color = p.fetch(:color) {nil}
+      part = p.fetch(:part) { :main }
+      type = p.fetch(:type) { "Creature"}
+
+      where[:card_decks] = {part: part} if part
+      where[:ctypes] = ["{#{type}}"] if type
+      where[:colors] = ["{#{color}}"] if color
+    end
+  end
+
+  def top_creatures
+    teste()
+  end
+
+  def top_instants
+    teste(type: "Instant")
+  end
+  
+  def most_playable_cards(limit = 10)
+      query = <<QUERY 
+SELECT cards.id, cards.name, count(cards.name) as quantity 
+FROM card_decks, cards  
+WHERE card_decks.part = 'main' 
+AND cards.id = card_decks.card_id
+AND 'Land' <> ANY(cards.ctypes)
+GROUP BY cards.name, cards.id
+ORDER BY quantity desc
+limit #{limit}
+QUERY
+
+    Card.find_by_sql(query)
+  end
+
+  def most_playable_decks(limit = 10)
     Deck.all
-      .select(:name, quantity)
+      .select(:name,Deck.arel_table[:name].count.as("quantity"))
       .group(:name)
-      .order(more_playable_to_less)
+      .order("quantity desc")
       .limit(limit)
   end
+
+  
 end
