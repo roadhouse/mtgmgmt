1570283897f03b7786471159ddefc4586350e148
Jean Uchoa <jean.uchoa@gmail.com>
2014-11-14 02:06:48 -0200

    add deckbuilder

--- lib/laracna/crawler.rb
+++ lib/laracna/crawler.rb
@@ -1,3 +1,36 @@
+class DeckBuilder
+  Standard = OpenStruct.new(parts: %i(main sideboard))
+
+  def initialize(format = Standard, params)
+    @card_list = params.delete(:card_list)
+    @deck      = Deck.new(params)
+    @format    = format
+  end
+
+  def build
+    @deck.tap do |deck|
+      populate
+
+      deck.save!
+    end
+  end
+
+  private
+
+  def populate
+    @format.parts.each { |part| add_cards_from(part) }
+  end
+
+  def add_cards_from(part)
+    @card_list.fetch(part).each do |attrs| 
+      name   = attrs.fetch(:card)
+      copies = attrs.fetch(:copies)
+
+      @deck.add_card(copies, name, part)
+    end
+
+  end
+end
 class Crawler
   def self.run!(site, page_range = 1..200, options = {})
     page_range.each do |page| 
