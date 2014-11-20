6abdaa7e87e69fd1bb29cb2edd67d55befacee4a
Jean Uchoa <jean.uchoa@gmail.com>
2014-11-20 00:48:46 -0200

    add search

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,10 +1,23 @@
 class CardsController < ApplicationController
+  respond_to :json, only: [:index]
+
   def index
-    @q = Card.search(params[:q])
-    @cards = @q.result
+    if search_parms
+      @cards =  Orthanc.new(search_parms.deep_symbolize_keys).cards
+    else
+      @cards = []
+    end
+
+    respond_with @cards
   end
 
   def show
     @card = Card.find(params[:id])
+  end
+
+  private
+
+  def search_parms
+    params.require(:query)
   end
 end
