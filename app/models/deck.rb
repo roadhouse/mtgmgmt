f720413726789892ef8172a490e1fbaa368306bd
Jean Uchoa <jean.uchoa@gmail.com>
2014-12-06 19:55:06 -0200

    remove methods

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -15,6 +15,8 @@ class Deck < ActiveRecord::Base
     Deck.where(table[:name].matches(name)).last
   end
 
+  #using by crawler
+  #move
   def add_card(copies, name, part)
     begin
       card_decks.build(card: Card.find_by_name!(name), copies: copies, part: part)
@@ -31,80 +33,11 @@ class Deck < ActiveRecord::Base
     for_game(:sideboard)
   end
 
-
-  def card_list_from(part)
-    card_decks.where(part: part).map do |entry| 
-      { copies: entry.copies, card: entry.card }
-    end
-  end
-
-  def for_game(part)
-    card_list_from(part).map do |card|
-      Array.new(card[:copies]) { |_| card[:card] }
-    end.flatten
-  end
-
   private
 
-  def group_by_card_type(part)
-    data = card_list_from(part).group_by do |deck_entry| 
-      deck_entry[:card].ctype.match(/Land|Instant|Sorcery|Enchantment|Planeswalker|Creature|Artifact/).to_s
-    end
-    
-    Hash[data.sort]
-  end
-end
-
-class DeckStats
-  TYPES =  {land: /Land/, instant: /Instant/, sorcery: /Sorcery/, enchantment: /Enchantment/, planeswalker: /Planeswalker/, creature: /Creature/, artifact: /Artifact/}
-
-  # [Card, Card]
-  def initialize(deck)
-    @deck = deck
-  end
-
-  def lands
-    grouped_by(:land)
-  end
-
-  def non_lands
-    @deck.find_all {|i| !i.ctype.match(TYPES[:land])}
-  end
-
-  def grouped_by(type)
-    type_string = TYPES[type]
-    @deck.find_all { |card| card.ctype.match(type_string) }
-  end
-
-  def by_manacost
-    x=non_lands.group_by {|i| Mana.new(i.mana_cost).converted_manacost }.sort
-    Hash[x]
-  end
-
-  def by_color
-    non_lands
-      .map {|i| Mana.new(i.mana_cost).colors}
-      .flatten
-      .group_by {|i| i}
-  end
-
-  def total_by_color
-    by_color.inject({}) {|m,v| m[v.first]=v.last.size;m}
-  end
-  
-  def total_by_manacost
-    by_manacost.inject({}) {|m,v| m[v.first]=v.last.size;m}
-  end
-
-  def total_by_type
-    group_by_card_type.inject({}) {|m,v| m[v.first]=v.last.size;m}
-  end
-  
-  def group_by_card_type
-    data = non_lands.group_by do |card| 
-      card.ctype.match(/Land|Instant|Sorcery|Enchantment|Planeswalker|Creature|Artifact/).to_s
-    end.sort
-    
-    Hash[data]
+  def for_game(part)
+    card_decks.from_part(part).map do |entry|
+      Array.new(entry.copies) { |_| entry.card }
+    end.flatten
   end
 end
