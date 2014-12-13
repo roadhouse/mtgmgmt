5723b84654c032234561994d2c7729acd5e6ff72
Jean Uchoa <jean.uchoa@gmail.com>
2014-12-13 13:26:06 -0200

    add specs

--- spec/factories/decks.rb
+++ spec/factories/decks.rb
@@ -1,7 +1,8 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
+  sequence(:url) { |n| "url#{n}" }
+  
   factory :deck do
-    name "MyString"
+    name "red deck wins"
+    url
   end
 end
--- spec/lib/orthanc/orthanc_spec.rb
+++ spec/lib/orthanc/orthanc_spec.rb
@@ -1,13 +1,24 @@
 require 'spec_helper'
 
 describe Orthanc do
-  # before(:all) do 
-    # create_list(:deck, 2)
-  # end
+  before(:all) do 
+    create_list(:deck, 2)
+  end
 
   let(:orthanc) { described_class.new({}) }
 
-  subject { orthanc.top_decks }
+  context ".top_decks" do
+    subject { orthanc.top_decks }
 
-  it { is_expected.to_not be_nil }
+    it { is_expected.to be_a ActiveRecord::Relation }
+
+    context "collection item " do 
+      subject { orthanc.top_decks.first }
+
+      it { is_expected.to be_a Deck  }
+
+      its(:name) { is_expected.to eq "red deck wins"  }
+      its(:quantity) { is_expected.to eq 2  }
+    end
+  end
 end
