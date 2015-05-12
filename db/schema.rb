a2577b0d4056f05c65c38dcb74629aaa9ddcfb7e
Jean Uchoa <jean.uchoa@gmail.com>
2015-05-12 03:59:00 -0300

    simplified view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150102203547) do
+ActiveRecord::Schema.define(version: 20150512061035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20150102203547) do
     t.date     "date"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.decimal  "price"
   end
 
   create_table "inventories", force: true do |t|
