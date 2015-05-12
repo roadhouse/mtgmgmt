275fdbfd66094720e47badf5ec3421dc63f87963
Jean Uchoa <jean.uchoa@gmail.com>
2015-05-12 00:38:10 -0300

    change tabs to accordion

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -21,3 +21,9 @@ app.controller('main_control', function($scope, $http, $timeout) {
 $(document).ready(function() {
     $('select').material_select();
 });
+
+  $(document).ready(function(){
+      $('.collapsible').collapsible({
+            accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+          });
+    });
