ccb1e33bcfe48aaceb9262ec018711636b4eb8b6
Jean Uchoa <jean.uchoa@gmail.com>
2015-05-17 20:15:12 -0300

    fix indent

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,14 +9,14 @@ $(function() {
 var app = angular.module('demos_cfg', []);
 
 app.controller('main_control', function($scope, $http, $timeout) {
-    $scope.search = null;
-    $scope.change = function(text) {
-      valtosend = $scope.search;
-      $http.get('/cards.json?query[name]=' + valtosend).then(function(result){
-    $scope.entries = result.data;
+  $scope.search = null;
+  $scope.change = function(text) {
+    valtosend = $scope.search;
+    $http.get('/cards.json?query[name]=' + valtosend).then(function(result){
+      $scope.entries = result.data;
     });
-    };
-  });
+  };
+});
 
 $(document).ready(function(){
   $(".button-collapse").sideNav();
