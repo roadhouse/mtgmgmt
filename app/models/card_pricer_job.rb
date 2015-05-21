47cc83369db0367a9dfee509dd5400abd1bd68d0
Jean Uchoa <jean.uchoa@gmail.com>
2015-05-21 18:12:04 -0300

    fix condition to retrieve price and add price source

--- app/models/card_pricer_job.rb
+++ app/models/card_pricer_job.rb
@@ -4,7 +4,8 @@ class CardPricerJob < Struct.new(:card_id)
     new_price = CardPricer.new(card.name).price
 
     if card.price != new_price
-      card.prices.build(value: card.price)
+      card.prices.build(value: card.price, source: "ligamagic") if card.price
+
       card.price = new_price
       card.price_updated_at = Time.zone.now
 
