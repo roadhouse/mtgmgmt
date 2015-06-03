a7d4eb39b60c2a0912cc986f77afe7f767cf8c1c
Jean Uchoa <jean.uchoa@gmail.com>
2015-06-03 17:30:59 -0300

    add ui feedback to current page and change layout on card search

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
   def render_ga
     render "shared/ga" if Rails.env.production?
   end
+
+  def navigation_item(name, url)
+    active_class = current_page?(url) ? {class: "active"} : {}
+
+    content_tag(:li, active_class) { link_to name, url }
+  end
 end
