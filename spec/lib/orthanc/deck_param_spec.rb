8edb2f7847099da72fb46302fdcfd576b6fc1741
Jean Uchoa <jean.uchoa@gmail.com>
2015-06-15 15:49:02 -0300

    add specs do deck_param

--- spec/lib/orthanc/deck_param_spec.rb
+++ spec/lib/orthanc/deck_param_spec.rb
@@ -1,6 +1,32 @@
 require 'spec_helper'
 
 describe DeckParam do
+  context "#season_is('season')" do
+    subject { described_class.new({}).season_is("season").to_sql }
+
+    it { is_expected.to eq %{"decks"."season" = 'season'} }
+  end
+
+  context "#name_quantity" do
+    subject { described_class.new({}).name_quantity.to_sql }
+
+    it { is_expected.to eq %{COUNT("decks"."name") AS quantity} }
+  end
+
+  context "#total_decks" do
+    subject { described_class.new({}).total_decks.to_sql }
+
+    it { is_expected.to eq %{SELECT COUNT("decks"."name") AS quantity FROM "decks"} }
+  end
+
+  context "#cards_on_deck" do
+    let(:sql_output) { %{SELECT jsonb_object_keys(list->'main') AS name FROM "decks" WHERE "decks"."season" = 'season'} }
+
+    subject { described_class.new({season: "season"}).cards_on_deck.to_sql }
+
+    it { is_expected.to eq sql_output }
+  end
+
   context "#params" do
     subject { described_class.new(filter_params).params }
 
@@ -15,11 +41,5 @@ describe DeckParam do
 
       it { expect { subject }.to raise_error KeyError }
     end
-  end
-
-  context "#season_is('season')" do
-    subject { described_class.new({}).season_is("season").to_sql }
-
-    it { is_expected.to eq %{"decks"."season" = 'season'} }
   end
 end
