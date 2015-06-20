1909ddd68b33bb43f72e3e9d7d6639df06fba380
Jean Uchoa <jean.uchoa@gmail.com>
2015-06-20 19:17:32 -0300

    change ortanc interface

--- app/presenters/meta_game_presenter.rb
+++ app/presenters/meta_game_presenter.rb
@@ -1,10 +1,10 @@
 class MetaGamePresenter
-  def top_cards(params = {})
+  def top_cards(params = "")
     Orthanc.new(params).top_cards
   end
 
   def top_decks
-    DeckPresenter.map Orthanc.new({}).top_decks.map {|d| d.archeptype_deck}
+    DeckPresenter.map Orthanc.new("").top_decks.map {|d| d.archeptype_deck}
   end
 
   private
