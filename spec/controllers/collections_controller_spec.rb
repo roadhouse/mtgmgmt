eb3133261e021576fedf583c5781c3ca5a6489a3
Jean Uchoa <jean.uchoa@gmail.com>
2015-07-15 18:04:41 -0300

    remove collection stuff from inventory controller

--- spec/controllers/collections_controller_spec.rb
+++ spec/controllers/collections_controller_spec.rb
@@ -7,7 +7,7 @@ describe CollectionsController, type: :controller do
 
     before do
       request.env["HTTP_REFERER"] = 'http://test.hostprevious_page'
-      put :update, id: collection.id, collection: { card_id: card.id, user_id: 1, copies: 4, list: "list" }
+      put :update, id: collection.id, collection: { card_id: card.id, user_id: 666, copies: 4, list: "list" }
     end
     
     context "response" do
@@ -23,7 +23,7 @@ describe CollectionsController, type: :controller do
       it { is_expected.to be_a Collection }
 
       its(:"class.count") { is_expected.to eq 1 }
-      its(:user_id) { is_expected.to eq 1 }
+      its(:user_id) { is_expected.to eq 666 }
       its(:name) { is_expected.to eq "list" }
 
       context "card list" do
@@ -35,12 +35,12 @@ describe CollectionsController, type: :controller do
 
         it "update current list" do
           request.env["HTTP_REFERER"] = 'http://test.hostprevious_page'
-          put :update, id: collection.id, collection: { card_id: another_card.id, user_id: 1, copies: 4, list: "list" }
+          put :update, id: collection.id, collection: { card_id: another_card.id, user_id: 666, copies: 4, list: "list" }
 
           expect(subject["Erase"]["total"]).to eq 4
           expect(subject["Erase"]["Theros"]["normal"]).to eq 4
 
-          expect(subject.keys).to eq ["Magma Jet", "Erase"]
+          expect(subject.keys).to eq ["Erase", "Roast", "Magma Jet"]
         end
       end
     end
--- spec/controllers/inventories_controller_spec.rb
+++ spec/controllers/inventories_controller_spec.rb
@@ -24,37 +24,6 @@ describe InventoriesController, type: :controller do
       its(:copies) { is_expected.to eq 4 }
       its(:list) { is_expected.to eq "list" }
     end
-
-    context "collection instance" do
-      let(:collection) { create(:collection, name: "list", user_id: 666) }
-      subject { assigns(:collection) }
-
-      it { is_expected.not_to be_nil }
-      it { is_expected.to be_a Collection }
-
-      its(:"class.count") { is_expected.to eq 1 }
-      its(:user_id) { is_expected.to eq 1 }
-      its(:name) { is_expected.to eq "list" }
-
-      context "card list" do
-        let(:another_card) { create(:card, name: "Erase") }
-        subject { assigns(:collection).list }
-
-        its(["Magma Jet", "total"]) { is_expected.to eq 4 }
-        its(["Magma Jet", "Theros", "normal"]) { is_expected.to eq 4 }
-
-        it "update current list" do
-          request.env["HTTP_REFERER"] = 'http://test.hostprevious_page'
-          put :update, id: inventory.id, inventory: { card_id: another_card.id, user_id: 1, copies: 4, list: "list" }
-
-          expect(subject["Erase"]["total"]).to eq 4
-          expect(subject["Erase"]["Theros"]["normal"]).to eq 4
-
-          expect(subject.keys).to eq ["Magma Jet", "Erase"]
-        end
-
-      end
-    end
   end
 
   context "POST on /create" do
@@ -82,33 +51,6 @@ describe InventoriesController, type: :controller do
       its(:card_id) { is_expected.to eq card.id }
       its(:copies) { is_expected.to eq 4 }
       its(:list) { is_expected.to eq "list" }
-    end
-
-    context "collection instance" do
-      subject { assigns(:collection) }
-
-      it { is_expected.not_to be_nil }
-      it { is_expected.to be_a Collection }
-
-      its(:"class.count") { is_expected.to eq 1 }
-      its(:user_id) { is_expected.to eq 1 }
-      its(:name) { is_expected.to eq "list" }
-
-      context "card list" do
-        let(:another_card) { create(:card, name: "Erase") }
-        subject { assigns(:collection).list }
-
-        its(["Magma Jet", "total"]) { is_expected.to eq 4 }
-        its(["Magma Jet", "Theros", "normal"]) { is_expected.to eq 4 }
-
-        it "update current list" do
-          request.env["HTTP_REFERER"] = 'http://test.hostprevious_page'
-          post :create, inventory: { card_id: another_card.id, user_id: 1, copies: 4, list: "list" }
-
-          expect(subject["Erase"]["total"]).to eq 4
-          expect(subject["Erase"]["Theros"]["normal"]).to eq 4
-        end
-      end
     end
   end
 end
