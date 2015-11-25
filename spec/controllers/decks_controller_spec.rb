51c7ffa4bb40fc8177f2042fe0d4a47a4d0b4303
Jean Uchoa <jean.uchoa@gmail.com>
2015-11-25 18:43:09 -0200

    add json specs

--- spec/controllers/decks_controller_spec.rb
+++ spec/controllers/decks_controller_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
-describe DecksController do
-  context "POST on /create"
+describe DecksController, type: :controller do
+  render_views
+
+  context "POST on /create" do 
+    let!(:card) { create(:card) }
+    let(:copies) { 4 }
+    before do
+      post :create, format: :json, deck: { card.id => copies }
+    end
+
+    context "response" do
+      subject { response }
+
+      it { is_expected.to be_successful }
+      its(:content_type) { is_expected.to eq "application/json" }
+
+      context "json data" do
+        let(:deck_list) { [{"copies" => copies, "name" => card.name, "id" => card.id}] }
+
+        subject { OpenStruct.new JSON.parse(response.body) }
+
+        its(:deck_list) { is_expected.to eq deck_list }
+      end
+    end
+  end
 end
--- spec/factories/card.rb
+++ spec/factories/card.rb
@@ -1,7 +1,27 @@
 FactoryGirl.define do
   factory :card do
-    name  "Magma Jet"
-    set   "THS"
-    printings ["Theros"]
+    name "Zulaport Cutthroat"
+    image "http//magiccards.info/scans/pt/bfz/126.jpg"
+    set "BFZ"
+    mana_cost "{1}{B}"
+    ctype "Creature — Human Rogue Ally"
+    power 1
+    toughness 1
+    rarity "Uncommon"
+    artist "Jason Rainville"
+    number 126
+    number_ex nil
+    original_type "Creature — Human Rogue Ally"
+    layout "normal"
+    border "black"
+    portuguese_name "Degolador de Zulaport"
+    original_text "Whenever Zulaport Cutthroat or another creature you control dies, each opponent loses 1 life and you gain 1 life."
+    flavor "\"Eldrazi? Ha! Try walking through Zulaport at night with your pockets full. Now that's dangerous.\""
+    loyalty 0
+    multiverse_id 402101
+    cmc 2
+    ctypes [ "Creature" ]
+    subtypes [ "Human" "Rogue" "Ally" ]
+    printings [ "BFZ" ]
   end
 end
