34d38b8e93c67ea557123e6c2e85c6df82a158d7
Jean Uchoa <jean.uchoa@gmail.com>
2015-11-30 00:24:24 -0200

    increase search result

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,7 +2,7 @@ class CardsController < ApplicationController
   respond_to :json, only: [:index]
 
   def index
-    @cards =  Orthanc.new(params[:query].to_s).cards.limit(5);
+    @cards =  Orthanc.new(params[:query].to_s).cards.limit(10);
 
     respond_with @cards
   end
