36a602ee5aa37030ab5054e93bb42056e0c51fd4
Jean Uchoa <jean.uchoa@gmail.com>
2015-12-21 16:43:37 -0200

    add js specs

--- spec/javascripts/app/live-search.controller.spec.js
+++ spec/javascripts/app/live-search.controller.spec.js
@@ -1,12 +1,32 @@
 //= require spec.helper
 'use strict';
 
-describe('LiveSearchController', function() {
+describe('DeckBuilderController', function() {
   it('define actions', function() {
     var $scope = {};
-    var controller = $controller('LiveSearchController', { $scope: $scope });
+    var controller = $controller('DeckBuilderController', { $scope: $scope });
 
-    expect($scope.addCardToDeck).toBeDefined();
-    expect($scope.change).toBeDefined();
+    expect($scope.addToDeck).toBeDefined();
+    expect($scope.removeToDeck).toBeDefined();
+    expect($scope.deckEntry).toBeDefined();
+    expect($scope.deckEntry).toBeDefined();
+  });
+
+  it('it add 4 cards to deck', function() {
+    var $scope = {};
+    var controller = $controller('DeckBuilderController', { $scope: $scope });
+
+    $scope.addToDeck(1);
+    expect($scope.deckEntry[1]).toBe(4);
+  });
+
+  it('it remove 4 cards from deck', function() {
+    var $scope = {};
+    var controller = $controller('DeckBuilderController', { $scope: $scope });
+
+    $scope.addToDeck(1);
+    $scope.removeToDeck(1);
+
+    expect($scope.deckEntry[1]).toBe(0);
   });
 });
