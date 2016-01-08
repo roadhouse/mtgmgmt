6d3fd6513908cf9ab9e31066300549a0e905c89c
Jean Uchoa <jean.uchoa@gmail.com>
2016-01-08 11:26:20 -0200

    vcr ignoring codeclimate

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,8 +1,9 @@
-require 'vcr'
+require "vcr"
 
 RSpec.configure do |config|
   VCR.configure do |c|
-    c.cassette_library_dir = 'spec/cassettes'
+    c.ignore_hosts "codeclimate.com"
+    c.cassette_library_dir = "spec/cassettes"
     c.configure_rspec_metadata!
     c.hook_into :webmock
   end
