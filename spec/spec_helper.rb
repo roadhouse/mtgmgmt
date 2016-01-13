3d29ad7216e114d4c28b4bc800f10d41e9ae9358
Jean Uchoa <jean.uchoa@gmail.com>
2016-01-13 17:54:47 -0200

    update config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.infer_base_class_for_anonymous_controllers = false
+  config.infer_spec_type_from_file_location!
   config.order = "random"
   config.use_transactional_fixtures = false
 end
