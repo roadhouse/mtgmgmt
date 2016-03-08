7be857759c8aba9944f393ac51b310a120c10e8e
Jean Uchoa <jean.uchoa@gmail.com>
2016-03-08 14:29:54 -0300

    enable partial double

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,9 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.infer_base_class_for_anonymous_controllers = false
   config.infer_spec_type_from_file_location!
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
   config.order = "random"
   config.use_transactional_fixtures = false
 end
