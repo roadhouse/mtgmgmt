a1a506ea00fa7b326113d69f2511ba07fc755cc6
Jean Uchoa <jean.uchoa@gmail.com>
2016-03-31 16:42:05 -0300

    fix capybara spec

--- spec/features/root_page_spec.rb
+++ spec/features/root_page_spec.rb
@@ -1,8 +1,12 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe "root page", type: :feature do
+  let(:deck) { create :deck }
+
+  before { deck.list["main"].keys.map { |k| create :card, name: k } }
+
   it "works" do
-    visit '/'
+    visit "/"
     expect(page).to have_content "welovemtg"
   end
 end
