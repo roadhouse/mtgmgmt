a43a24ba300f91e013869642a551f8a845a20981
Jean Uchoa <jean.uchoa@gmail.com>
2016-03-31 19:52:26 -0300

    fix specs to mtgo index page class

--- lib/laracna/mtgo/index_page.rb
+++ lib/laracna/mtgo/index_page.rb
@@ -4,15 +4,13 @@ require "open-uri"
 module Laracna
   module Mtgo
     class IndexPage
-      def initialize(page)
-        @document = Nokogiri::HTML open page
-      end
+      def initialize(page); end
 
-      def decks_ids
-        @document.search("tr strong a")
-          .map { |node| node.attribute("href").text }
-          .map { |url| url.gsub(PageUrl::DECK_URL, "") }
-          .map(&:to_i)
+      def urls
+        (8.months.ago.to_date..DateTime.now.to_date).to_a.reverse.map do |date|
+          formated_date = date.strftime("%Y-%m-%d")
+          "http://magic.wizards.com/en/articles/archive/mtgo-standings/standard-daily-#{formated_date}"
+        end
       end
     end
   end
--- spec/lib/laracna/mtgo/index_page_spec.rb
+++ spec/lib/laracna/mtgo/index_page_spec.rb
@@ -3,20 +3,10 @@ require "rspec/its"
 
 require "./lib/laracna/laracna"
 
-describe Laracna::Scg::IndexPage, :vcr do
+describe Laracna::Mtgo::IndexPage, :vcr do
   context "#deck_nodes" do
-    subject { described_class.new(1).deck_nodes }
+    subject { described_class.new(1).urls }
 
     it { is_expected.to be_a Array }
-    its(:size) { is_expected.to be_eql 100 }
-  end
-
-  context "#decks_ids" do
-    subject { described_class.new(1).decks_ids }
-
-    it { is_expected.to be_a Array }
-    its(:size) { is_expected.to be_eql 100 }
-    its(:first) { is_expected.to be_a Integer }
-    its(:last) { is_expected.to be_a Integer }
   end
 end
