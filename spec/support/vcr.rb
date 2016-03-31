65bf126ad3338256a7b7e8e859b9e1e9de77c1b9
Jean Uchoa <jean.uchoa@gmail.com>
2016-03-31 18:37:40 -0300

    fix rubocop warnings

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,8 +1,8 @@
 require "vcr"
 
-RSpec.configure do |config|
+RSpec.configure do
   VCR.configure do |c|
-    c.default_cassette_options = { :record => :once, re_record_interval: 1.month }
+    c.default_cassette_options = {record: :once, re_record_interval: 1.month}
     c.ignore_hosts "codeclimate.com"
     c.cassette_library_dir = "spec/cassettes"
     c.configure_rspec_metadata!
