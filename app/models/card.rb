2f1d888c9fd0bc926b1f1edbcd2f07051ad07ea8
Jean Uchoa <jean.uchoa@gmail.com>
2016-04-11 15:35:28 -0300

    remove crap code

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,36 +1,26 @@
 class Card < ActiveRecord::Base
-  ICON_NAME = {
-    KTK: "khans-of-tarkir",
-    FRF: "fate-reforged",
-    DTK: "dragons-of-tarkir",
-    ORI: "magic-origins",
-    BFZ: "battle-for-zendikar"
-  }
-
   scope :per_name, ->(name) { where(["name ilike ?", "%#{name}%"]) }
   scope :per_type, ->(type) { where(["ctype ilike ?", "%#{type}%"]) }
 
-  validates :name,
-    uniqueness: true,
-    presence: true
+  validates :name, uniqueness: true, presence: true
 
   has_many :inventories
   has_many :users, through: :inventories
   has_many :prices
 
   def portuguese_name
-    super or name
+    super || name
   end
 
   def self.find_by_name!(name)
-    per_name(name).first or Card.create!(name: name, set: :fake)
+    per_name(name).first || Card.create!(name: name, set: :fake)
   end
 
   def on_demand_price
     # if price_updated_at.nil? or price_updated_at < 1.day.ago
-      # Delayed::Job.enqueue CardPricerJob.new(self.id)
+    #   Delayed::Job.enqueue CardPricerJob.new(self.id)
     # end
 
-    self.price
+    price
   end
 end
