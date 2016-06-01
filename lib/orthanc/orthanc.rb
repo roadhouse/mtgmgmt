6500a9292b420efa875b14fe880eb28eeff78eaf
Jean Uchoa <jean.uchoa@gmail.com>
2016-05-31 22:00:25 -0300

    refactoring

--- lib/orthanc/orthanc.rb
+++ lib/orthanc/orthanc.rb
@@ -7,15 +7,11 @@ require_dependency "param_builder"
 
 class Orthanc
   SEASON = "BFZ-DTK-ORI-OWG-SOI"
+  DEFAULT_OPTIONS = { limit: 12, season: SEASON, part: "main" }
 
   def initialize(options = nil)
-    default_options = {
-      limit: 12,
-      season: SEASON,
-      part: "main"
-    }
 
-    @options = default_options.merge ParamBuilder.params(options.to_s)
+    @options = DEFAULT_OPTIONS.merge ParamBuilder.params(options.to_s)
 
     @card = CardParam.new(@options)
     @deck = DeckParam.new(@options)
@@ -30,42 +26,31 @@ class Orthanc
   # top cards played in standard
   # DEFAULT: looking in main deck and ignore land cards
   def top_cards
-    @card.table
-      .with({metagame: metagame})
+    cards
+      .where(@card.not_lands)
+      .with(metagame: metagame)
       .joins("INNER JOIN metagame ON metagame.card = cards.name")
-      .where(@card.params).where(@card.not_lands)
       .order("metagame.quantity DESC")
-      .limit(@options.fetch(:limit))
+      .limit(@options.fetch :limit)
   end
 
+  # the relation between the decks of the metagame and the card's quantity in this decks
   def metagame
-    # SELECT count(card) as quantity, card
-    # FROM decks
-    # CROSS JOIN LATERAL jsonb_object_keys(list->'main') as card
-    # WHERE decks.season = 'BFZ-DTK-ORI-OWG-SOI'
-    # GROUP BY card
-
     @deck.table
       .select('count(card) as quantity, card')
-      .joins("CROSS JOIN LATERAL jsonb_object_keys(list->'#{@options.fetch(:part)}') as card")
+      .joins("CROSS JOIN LATERAL jsonb_object_keys(list->'#{@options.fetch :part}') as card")
       .where(@deck.params)
       .group('card')
   end
 
-  # top cards played in standard, in the last season
-  # find deck by card
-  # SELECT *
-  # FROM decks
-  # CROSS JOIN LATERAL jsonb_object_keys(list->'main') as card
-  # where card like 'Ojutai%'
-  # GROUP BY id, card
+  # top decks from a season
   def top_decks
     @deck.table
       .select(@deck.all_fields, "COUNT(decks.list->'main_cards') AS quantity")
       .group(:id)
       .order("COUNT(decks.list->'main_cards') DESC")
       .where(@deck.params)
-      .limit(@options.fetch(:limit))
+      .limit(@options.fetch :limit)
   end
 
   # list cards from users
@@ -73,7 +58,7 @@ class Orthanc
     user.inventories
       .where(@inventory.params)
       .joins(:card)
-      .merge(cards.order(price: :desc))
-      .limit(@options.fetch(:limit))
+      .merge(cards.order price: :desc)
+      .limit(@options.fetch :limit)
   end
 end
