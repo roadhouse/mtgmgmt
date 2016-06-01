d957d20c858406e655ab05d99b689dd61697ba23
Jean Uchoa <jean.uchoa@gmail.com>
2016-06-01 18:12:17 -0300

    remove specs

--- spec/lib/laracna/mtgdecks/index_page_spec.rb
+++ spec/lib/laracna/mtgdecks/index_page_spec.rb
@@ -4,11 +4,5 @@ require "rspec/its"
 require "./lib/laracna/laracna"
 
 describe Laracna::Mtgdecks::IndexPage, :vcr do
-  let(:decks_list_url) { "http://www.mtgdecks.net/decks/viewByFormat/39/page:1" }
 
-  context "#urls" do
-    subject { described_class.new(decks_list_url).urls }
-    it { is_expected.to be_a Array }
-    it { is_expected.to_not be_empty }
-  end
 end
