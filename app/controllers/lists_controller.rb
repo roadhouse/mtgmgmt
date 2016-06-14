781ec108c54f8c05527bcf786f92f38af1febc89
Jean Uchoa <jean.uchoa@gmail.com>
2016-06-14 19:16:20 -0300

    refactoring

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,6 +3,12 @@ class ListsController < ApplicationController
   respond_to :json, only: [:show]
 
   def show
-    @inventories = Orthanc.new("list:#{params[:name]} #{params[:query]}").from_user User.find(params[:user_id])
+    @inventories = Orthanc.new("list:#{params[:name]} #{params[:query]}").from_user user
+  end
+
+  private
+
+  def user
+    User.find params[:user_id]
   end
 end
