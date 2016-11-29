48e5e8d4af1a9501a8599b4323bee9f12fe455db
Jean Uchoa <jean.uchoa@gmail.com>
2016-11-29 15:45:38 -0200

    refactoring mtg api card data conversion

--- lib/laracna/card_conversor.rb
+++ lib/laracna/card_conversor.rb
@@ -33,17 +33,3 @@ class CardConversor
     @api_data.tap { |card| ATTRS_TO_DELETE.map { |attr| card.delete attr } }
   end
 end
-
-class RepoWrapper
-  def initialize(set)
-    @set = set
-  end
-
-  def api_data
-    MTG::Card.where(set: @set).all
-  end
-
-  def attributes
-    api_data.map { |data| CardConversor.convert data }
-  end
-end
---
+++ spec/lib/laracna/card_conversor_spec.rb
@@ -0,0 +1,36 @@
+require "./lib/laracna/laracna"
+require "mtg_sdk"
+require "./spec/support/vcr"
+require "rspec/its"
+
+describe CardConversor do
+  let(:card_data) { VCR.use_cassette("kaladesh-set") { MTG::Card.where(set: 'KLD').all } }
+  let(:card_conversor) { described_class.new card_data.first }
+  let(:model_attributes) { described_class::EQUIVALENT_NAMES.keys }
+  let(:original_fields) { described_class::ATTRS_TO_DELETE }
+  let(:original_types) { described_class::EQUIVALENT_NAMES.values }
+
+  context "#normalize_data" do
+    subject { card_conversor.normalize_data }
+
+    it { is_expected.to be_a Hash }
+    its(:keys) { is_expected.to include(*model_attributes) }
+    its(:keys) { is_expected.not_to include(*original_types) }
+  end
+
+  context "#delete_fields" do
+    subject { card_conversor.delete_fields }
+
+    it { is_expected.to be_a Hash }
+    its(:keys) { is_expected.not_to include(*original_fields) }
+  end
+
+  context '#convert' do
+    subject { card_conversor.convert }
+
+    it { is_expected.to be_a Hash }
+    its(:keys) { is_expected.to include(*model_attributes) }
+    its(:keys) { is_expected.not_to include(*original_types) }
+    its(:keys) { is_expected.not_to include(*original_fields) }
+  end
+end
