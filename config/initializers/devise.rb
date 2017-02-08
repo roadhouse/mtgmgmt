f2ec43557a37c1edb017d1f678aaded65ab1c951
Jean Uchoa <jean.uchoa@gmail.com>
2017-02-08 16:40:09 -0200

    update devise's secret

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,6 +8,7 @@ Devise.setup do |config|
   config.password_length = 8..72
   config.reconfirmable = true
   config.reset_password_within = 6.hours
+  config.secret_key = '0b9084046574a4ff6646556831422494482c4508f35a365d4b81ed5be386db24b43c76181054ddd4e6d07a777bf8c69f33f21dd3af34b514f7d9fe7b98031af4'
   config.sign_out_via = :get
   config.skip_session_storage = [:http_auth]
   config.stretches = Rails.env.test? ? 1 : 10
