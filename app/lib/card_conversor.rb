40e945c58343fee8ba4a8d12cf062d47a16e93fb
Jean Uchoa <jean.uchoa@gmail.com>
2018-06-18 16:27:21 -0300

    update code

--- app/lib/card_conversor.rb
+++ app/lib/card_conversor.rb
@@ -1,17 +1,17 @@
 class CardConversor
-  ATTRS_TO_DELETE = %i(id mciNumber watermark variations)
+  ATTRS_TO_DELETE = %w(foreignNames types rulings legalities variations type text id starter setName)
   EQUIVALENT_NAMES = {
-    color_identity: :colorIdentity,
-    image: :imageName,
-    mana_cost: :manaCost,
-    multiverse_id: :multiverseid,
-    original_text: :text,
-    original_type: :type,
-    ctypes: :types,
+    ctype: "type",
+    image: "imageUrl",
+    mana_cost: "manaCost",
+    multiverse_id: "multiverseid",
+    original_text: "originalText",
+    original_type: "originalType",
+
   }
 
   def initialize(api_data)
-    @api_data = api_data
+    @api_data = api_data.to_hash
   end
 
   def self.convert(api_data)
@@ -25,11 +25,11 @@ class CardConversor
 
   def normalize_data
     @api_data.tap do |data|
-      EQUIVALENT_NAMES.each_pair { |ar_field, api_field| data[ar_field] = data.delete api_field }
+      EQUIVALENT_NAMES.each_pair { |ar_field, api_field| data[ar_field] = data.delete api_field  }
     end
   end
 
   def delete_fields
-    @api_data.tap { |card| ATTRS_TO_DELETE.map { |attr| card.delete attr } }
+    @api_data.tap { |card| ATTRS_TO_DELETE.map { |attr| card.delete attr  }  }
   end
 end
